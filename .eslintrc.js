d1a4800cc288b1f728913715bb963c1ff7af074e
Arnaud Ferrand <arferrand@gmail.com>
2020-10-21 18:20:12 +0100

    adds eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  // plugins: ['jest', 'prettier'],
+  // rules: {
+  //   'prettier/prettier': 'error',
+  // },
+  // env: {
+  //   node: true,
+  //   'jest/globals': true,
+  // },
+};
